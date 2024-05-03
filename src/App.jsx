c470706e5ea478b5087ab0541f44937c5fcbef80
nnguyentu13@gmail.com <nnguyentu13@gmail.com>
2024-05-03 16:01:45 +0700

    bla bla bla

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
-import Button from '@mui/material/Button'
+import { FormControl, MenuItem, Select } from '@mui/base'
+import { LightModeIcon, DarkModeOutlinedIcon, SettingsBrightnessIcon } from '@mui/icons-material'
+import { InputLabel, useColorScheme } from '@mui/material'
+import Box from '@mui/material/Box'
 
-function App() {
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
 
   return (
-    <>
-      <div>shit</div>
-      <Button variant='text'>Text</Button>
-      <Button variant='contained'>Hello World</Button>
-      <Button variant='outlined'>Outlined</Button>
-    </>
+    <FormControl sx={{ m:1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <LightModeIcon fontSize="small" /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeOutlinedIcon fontSize="small" /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+            <SettingsBrightnessIcon fontSize="small" /> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
   )
+}
+
+function App() {
+  return (<ModeSelect />)
 }
 
 export default App
