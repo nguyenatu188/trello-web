af44344289bbfd8fd4fcd9a3a9f7dd84e7002da4
nnguyentu13@gmail.com <nnguyentu13@gmail.com>
2024-05-13 17:21:00 +0700

    du ma

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -18,13 +18,26 @@ function ModeSelect() {
 
   return (
     <FormControl size="small">
-      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <InputLabel
+        id="label-select-dark-light-mode"
+        sx={{
+          color: 'white',
+          '&.Mui-focused': { color: 'white' }
+        }}
+      >Mode</InputLabel>
       <Select
         labelid="label-select-dark-light-mode"
         id="select-dark-light-mode"
         value={mode}
         label="Mode"
         onChange={handleChange}
+        sx={{
+          color: 'white',
+          '.MuiOutlinedInput-notchedOutline': { borderColor: 'white' },
+          '&:hover .MuiOutlinedInput-notchedOutline': { borderColor: 'white' },
+          '&.Mui-focused .MuiOutlinedInput-notchedOutline': { borderColor: 'white' },
+          '.MuiSvgIcon-root': { color: 'white' }
+        }}
       >
         <MenuItem value="light" sx={{ minWidth: '120px' }}>
           <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
